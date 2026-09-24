470c0f88eb6b174579dbf3e9292f60c3fb6df4eb
MrinalSingh04 <mrinalsingh7000@gmail.com>
2026-09-24 22:46:08 +0530

    solved some loops qs

--- src/DSA/Basics/LoopsPractice.java
+++ src/DSA/Basics/LoopsPractice.java
@@ -45,27 +45,37 @@ public class LoopsPractice {
 //        }
 
 
-        int n = 5;
+//        int n = 5;
+//
+//        for (int i = 1; i <= n; i++) {
+//
+//            // Spaces before stars
+//            for (int j = 1; j <= n - i; j++) {
+//                System.out.print(" ");
+//            }
+//
+//            // Stars and inner spaces
+//            for (int j = 1; j <= 2 * i - 1; j++) {
+//
+//                if (j == 1 || j == 2 * i - 1 || i == n) {
+//                    System.out.print("*");
+//                } else {
+//                    System.out.print(" ");
+//                }
+//            }
+//
+//            System.out.println();
+//        }
 
-        for (int i = 1; i <= n; i++) {
 
-            // Spaces before stars
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
+        //Print even numbers from 1 to 100
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 2 == 0) {
+//                System.out.println(i);
+//            }
+//        }
 
-            // Stars and inner spaces
-            for (int j = 1; j <= 2 * i - 1; j++) {
 
-                if (j == 1 || j == 2 * i - 1 || i == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-
-            System.out.println();
-        }
 
     }
 }
---
+++ src/DSA/PracticeProblems/Revision.java
@@ -0,0 +1,9 @@
+package DSA.PracticeProblems;
+
+public class Revision {
+    public static void main(String[] args) {
+
+        // swap 2 numbers
+
+    }
+}
